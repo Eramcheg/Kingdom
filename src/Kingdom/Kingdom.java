53faa903e95919291ba60ec0f0fa9c38725f2e90
Eramcheg <eramcheg@gmail.com>
2022-08-25 11:49:45 +0200

    Full level of Monster

--- src/Kingdom/Kingdom.java
+++ src/Kingdom/Kingdom.java
@@ -13,8 +13,30 @@ import java.util.Timer;
 public class Kingdom implements ActionListener {
    public static JLabel[][] jlab = {{new JLabel("X"),new JLabel("X"),new JLabel("X"),new JLabel("X"),new JLabel("X")},{new JLabel("X"),new JLabel("X"),new JLabel("X"),new JLabel("X"),new JLabel("X")},{new JLabel("X"),new JLabel("X"),new JLabel("X"),new JLabel("X"),new JLabel("X")},{new JLabel("X"),new JLabel("X"),new JLabel("X"),new JLabel("X"),new JLabel("X")},{new JLabel("X"),new JLabel("X"),new JLabel("X"),new JLabel("X"),new JLabel("X")}};
 
-    static int Xid=0,Yid=0;
-    static int Xmonster=4,Ymonster=4;
+
+   private static int Xid=0,Yid=0;
+    private static int Xmonster=4,Ymonster=4;
+    private static int Xdef=4,Ydef=4;
+
+    private static final double defaultAttack=0.0;
+    private static final double defaultHp=10.0;
+
+
+
+
+
+
+    static boolean isPause=false;
+    static JButton TryAgain =new JButton("Try Again");
+    static JButton Left=new JButton("<--");
+    static JButton Up=new JButton("|/");
+    static JButton Down=new JButton("/|");
+    static JButton Right=new JButton("-->");
+
+
+
+
+
     Kingdom()
     {
         JFrame jfrm = new JFrame("A Simple Swing Application");
@@ -29,18 +51,20 @@ public class Kingdom implements ActionListener {
                 jfrm.add(jlab[i][j]);
             }
         }
-        JButton Left=new JButton("<--");
-        JButton Up=new JButton("|/");
-        JButton Down=new JButton("/|");
-        JButton Right=new JButton("-->");
+
+
         Left.addActionListener( this);
         Up.addActionListener(this);
         Down.addActionListener(this);
         Right.addActionListener(this);
+        TryAgain.addActionListener(this);
+
         jfrm.add(Left);
         jfrm.add(Up);
         jfrm.add(Down);
         jfrm.add(Right);
+        jfrm.add(TryAgain);
+        TryAgain.setVisible(false);
         jfrm.setVisible(true);
     }
 
@@ -53,42 +77,68 @@ public class Kingdom implements ActionListener {
         }});
 
 
-        findCoords();                               //Код ниже
-        TimerTask timerTask=new TimerTask() {
-            @Override
-            public void run() {
-                jlab[Xmonster][Ymonster].setText("X");
-                jlab[Xmonster][Ymonster].setForeground(Color.BLACK);
-                int X=Xmonster-Xid;
-                int Y=Ymonster-Yid;
-                if(X>0)
-                {//if(Xmonster!=0)
-                    Xmonster--;}
-                else if(X<0)
-                {//if(Xmonster!=4)
-                        Xmonster++;}
-                else if (X==0) {
-                    if(Y>0)
+        //findCoords();                               //Код ниже
+
+        Monster m1=new Monster("It",Xmonster,Ymonster,jlab);
+        Human h1=new Human("Adam",Xid,Yid,jlab);
+
+
+
+
+
+
+
+        TimerTaskDoLogicOfMonsterWithTimer:             // метка логика монстра таймер
+        {
+            TimerTask timerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    if(isPause)
                     {
-                        Ymonster--;
-                    } else if (Y<0) {
-                        Ymonster++;
-                    } else if (Y==0) {
-                        System.out.println("GOT YOU");
+                        BlockButtons();
+                        TryAgain.setVisible(true);
+                    }
+                    if (isPause == false) {
+                        TryAgain.setVisible(false);
+                        jlab[Xmonster][Ymonster].setText("X");
+                        jlab[Xmonster][Ymonster].setForeground(Color.BLACK);
+                        int X = Xmonster - Xid;
+                        int Y = Ymonster - Yid;
+                        if (X > 0) {
+                            Xmonster--;
+                        } else if (X < 0) {
+                            Xmonster++;
+                        }
+                        if (Y > 0) {
+                            Ymonster--;
+                        } else if (Y < 0) {
+                            Ymonster++;
+                        }
+
+                        if (X == 0 && Y == 0) {
+                            Eraser();
+                            isPause=true;
+
+                        }
+
+                        jlab[Xmonster][Ymonster].setText("M");
+                        jlab[Xmonster][Ymonster].setForeground(Color.RED);
+
                     }
                 }
+            };
+
+            Timer timer=new Timer();
+            timer.schedule(timerTask,1000,550);
+        }
 
 
 
-                jlab[Xmonster][Ymonster].setText("M");
-                jlab[Xmonster][Ymonster].setForeground(Color.RED);
 
-            }
-        };
-        Timer timer=new Timer();
-        timer.schedule(timerTask,1000,750);
-        Monster m1=new Monster("It",4,4,jlab);
-        Human h1=new Human("Adam",0,0,jlab);
+
+
+
+
 
         System.out.println("Kingdom was succesfully founded");
 
@@ -121,15 +171,32 @@ public class Kingdom implements ActionListener {
                 jlab[Xid][Yid + 1].setForeground(Color.BLUE);
             }
         }
-        else
+        else if(actionEvent.getActionCommand().equals("<--"))
         {   if(Yid!=0) {
             jlab[Xid][Yid].setText("X");
             jlab[Xid][Yid-1].setText("H");
             jlab[Xid][Yid - 1].setForeground(Color.BLUE);
                         }
         }
+        else {
+                UnBlockButtons();
+                isPause=false;
+        }
         jlab[Xid][Yid].setForeground(Color.BLACK);
 
+        }
+        public static void Eraser(){
+        for(int i=0;i<5;i++)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    jlab[i][j].setText("X");
+                }
+            }
+        jlab[0][0].setText("H");
+        jlab[Xdef][Ydef].setText("M");
+        Xmonster=Xdef;
+        Ymonster=Ydef;
         }
         public static void findCoords()  // Метод нахождения позиции Монстра и Игрока
         {
@@ -150,6 +217,21 @@ public class Kingdom implements ActionListener {
                 }
             }
         }
+        public static void BlockButtons()
+        {
+            Right.setVisible(false);
+            Left.setVisible(false);
+            Up.setVisible(false);
+            Down.setVisible(false);
+        }
+        public static void UnBlockButtons()
+    {
+        Right.setVisible(true);
+        Left.setVisible(true);
+        Up.setVisible(true);
+        Down.setVisible(true);
+    }
+
     }
 
 
